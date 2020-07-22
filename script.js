a4faf706d43897e54dd786b8d020eff9c4740b62
GameJutsu <GameJutsu@users.noreply.github.com>
2020-07-22 18:00:08 +0530

    Updated Color Display
    Updated color display to use three distinct codes instead of a single string

--- script.js
+++ script.js
@@ -9,127 +9,116 @@ var squares = document.querySelectorAll(".square");
 var message = document.querySelector("#message");
 var playAgainButton = document.querySelector("#playAgainButton");
 var modeButtons = document.querySelectorAll(".mode");
+var colorCodeHead = [];
 
 init();
 
-function init()
-{
-	modeButtons[1].classList.add("selected");
-	playAgainButton.addEventListener("click", playAgain);
-	setupModeButtons();
-	playAgain();
+function init() {
+  modeButtons[1].classList.add("selected");
+  playAgainButton.addEventListener("click", playAgain);
+  setupModeButtons();
+  playAgain();
 }
 
-function setupModeButtons()
-{
-	for(var i = 0; i < 2; i++)
-	{
-		modeButtons[i].addEventListener("click", function()
-		{
-			this.classList.add("selected");
-	 	//Using indexOf because i changes to 2 after loading
-	 	modeButtons[1 - (modeButtons.indexOf(this))].classList.remove("selected");
-	 	difficulty = 3 + (3 * modeButtons.indexOf(this));
-	 	playAgain();
-	 	if(modeButtons.indexOf(this) == 0)
-	 	{
-	 		for(var j = 3; j < squares.length; j++)
-	 		{
-	 			squares[j].style.backgroundColor = bgcolor;
-	 		}
-	 	}
-	 });
-	}
+function setupModeButtons() {
+  for (var i = 0; i < 2; i++) {
+    modeButtons[i].addEventListener("click", function () {
+      this.classList.add("selected");
+      //Using indexOf because i changes to 2 after loading
+      modeButtons[1 - modeButtons.indexOf(this)].classList.remove("selected");
+      difficulty = 3 + 3 * modeButtons.indexOf(this);
+      playAgain();
+      if (modeButtons.indexOf(this) == 0) {
+        for (var j = 3; j < squares.length; j++) {
+          squares[j].style.backgroundColor = bgcolor;
+        }
+      }
+    });
+  }
 }
 
-function pickRandom(min, max)
-{
-	return Math.floor(Math.random() * (max - min + 1));
+function pickRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1));
 }
 
-function generateRandomColors(difficulty)
-{
-	var colors = [];
-	for(var i=0; i < difficulty; i++)
-	{
-		colors.push("rgb(" + pickRandom(0, 255) + ", " + pickRandom(0, 255) + ", " + pickRandom(0, 255) + ")");
-	}
-	return colors;
+function generateRandomColors(difficulty) {
+  var colors = [];
+  var pickedNumber = pickRandom(0, difficulty - 1);
+  for (var i = 0; i < difficulty; i++) {
+    var colorCode = [];
+    for (var j = 0; j < 3; j++) {
+      colorCode.push(pickRandom(0, 255));
+      if (i == pickedNumber) {
+        colorCodeHead = colorCode;
+      }
+    }
+    colors.push(
+      "rgb(" + colorCode[0] + ", " + colorCode[1] + ", " + colorCode[2] + ")"
+    );
+  }
+  colors[difficulty] = pickedNumber;
+  return colors;
 }
 
-function setupSquares()
-{
-	for(var i=0; i < difficulty; i++)
-	{
-		squares[i].style.backgroundColor = colors[i];
-		squares[i].addEventListener("click", function()
-		{
-			if(difficulty != 3 || squares.indexOf(this) <= 2)
-			{
-				if(this.style.backgroundColor == pickedColor)
-				{
-					for(var i=0; i < difficulty; i++)
-					{
-						squares[i].style.backgroundColor = pickedColor;
-					}
-					message.textContent = "Correct!";
-					message.style.color = "green";
-					document.querySelector("h1").style.backgroundColor = pickedColor;
-					// playAgainButton.style.color = pickedColor;
-					// modeButtons[(difficulty - 3) / 3].style.backgroundColor = pickedColor;
-					// modeButtons[1 - (difficulty - 3) / 3].style.color = pickedColor;
-					playAgainButton.textContent = "Play Again";
-					document.querySelectorAll(".hover").forEach(function(el)
-					{
-						el.style.setProperty("--hoverColor", pickedColor);
-					});
-					document.querySelectorAll("button").forEach(function(el, i)
-					{
-						el.style.setProperty("--buttonColor", pickedColor);
-						if(i == ((difficulty - 3) / 3) + 1)
-						{
-							el.style.backgroundColor = pickedColor;
-						}
-					});
-				}
-				else
-				{
-					this.style.backgroundColor = bgcolor;
-					message.textContent = "Try Again";
-					message.style.color = "red";
-					playAgainButton.textContent = "New Colors";	
-				}
-			}
-		});
-	}
+function setupSquares() {
+  for (var i = 0; i < difficulty; i++) {
+    squares[i].style.backgroundColor = colors[i];
+    squares[i].addEventListener("click", function () {
+      if (difficulty != 3 || squares.indexOf(this) <= 2) {
+        if (this.style.backgroundColor == pickedColor) {
+          for (var i = 0; i < difficulty; i++) {
+            squares[i].style.backgroundColor = pickedColor;
+          }
+          message.textContent = "Correct!";
+          message.style.color = "green";
+          document.querySelector("h1").style.backgroundColor = pickedColor;
+          // playAgainButton.style.color = pickedColor;
+          // modeButtons[(difficulty - 3) / 3].style.backgroundColor = pickedColor;
+          // modeButtons[1 - (difficulty - 3) / 3].style.color = pickedColor;
+          playAgainButton.textContent = "Play Again";
+          document.querySelectorAll(".hover").forEach(function (el) {
+            el.style.setProperty("--hoverColor", pickedColor);
+          });
+          document.querySelectorAll("button").forEach(function (el, i) {
+            el.style.setProperty("--buttonColor", pickedColor);
+            if (i == (difficulty - 3) / 3 + 1) {
+              el.style.backgroundColor = pickedColor;
+            }
+          });
+        } else {
+          this.style.backgroundColor = bgcolor;
+          message.textContent = "Try Again";
+          message.style.color = "red";
+          playAgainButton.textContent = "New Colors";
+        }
+      }
+    });
+  }
 }
 
-function playAgain()
-{
-	// playAgainButton.style.color = "";
-	// modeButtons[(difficulty - 3) / 3].style.backgroundColor = "";
-	// modeButtons[1 - (difficulty - 3) / 3].style.color = "";
-	document.querySelectorAll(".hover").forEach(function(el)
-	{
-		el.style.setProperty("--hoverColor", defColor);
-	});
-	document.querySelectorAll("button").forEach(function(el, i)
-	{
-		el.style.setProperty("--buttonColor", defColor);
-		if(i == ((difficulty - 3) / 3) + 1)
-		{
-			el.style.backgroundColor = defColor;
-		}
-		else
-		{
-			el.style.backgroundColor = "";
-		}
-	});
-	playAgainButton.textContent = "New Colors";
-	document.querySelector("h1").style.backgroundColor = "";
-	message.textContent = "";
-	colors = generateRandomColors(difficulty);
-	pickedColor = colors[pickRandom(0, difficulty - 1)];
-	setupSquares();
-	document.querySelector("#colorDisplay").textContent = pickedColor.toUpperCase();
+function playAgain() {
+  // playAgainButton.style.color = "";
+  // modeButtons[(difficulty - 3) / 3].style.backgroundColor = "";
+  // modeButtons[1 - (difficulty - 3) / 3].style.color = "";
+  document.querySelectorAll(".hover").forEach(function (el) {
+    el.style.setProperty("--hoverColor", defColor);
+  });
+  document.querySelectorAll("button").forEach(function (el, i) {
+    el.style.setProperty("--buttonColor", defColor);
+    if (i == (difficulty - 3) / 3 + 1) {
+      el.style.backgroundColor = defColor;
+    } else {
+      el.style.backgroundColor = "";
+    }
+  });
+  playAgainButton.textContent = "New Colors";
+  document.querySelector("h1").style.backgroundColor = "";
+  message.textContent = "";
+  colors = generateRandomColors(difficulty);
+  pickedColor = colors[colors[difficulty]];
+  setupSquares();
+  // document.querySelector("#colorDisplay").textContent = pickedColor.toUpperCase();
+  document.getElementById("red").textContent = colorCodeHead[0];
+  document.getElementById("green").textContent = colorCodeHead[1];
+  document.getElementById("blue").textContent = colorCodeHead[2];
 }
